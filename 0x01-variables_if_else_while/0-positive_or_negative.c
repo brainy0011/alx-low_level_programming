fcdeb666fe92219726a36e8e7e60b95b393b1e1f
brainy <gabrielakinbi@gmail.com>
2023-07-17 08:16:01 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
@@ -22,6 +22,7 @@
          else if (n == 0)
                  printf("%d is zero\n", n);
          else
-                 printf("%d is negative\n", n);
-	 return (0);
+                 printf("%d is negative", n);
+	 printf("\n");
+	 return(0);
 }
